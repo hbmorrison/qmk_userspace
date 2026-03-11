33cb34a38122e4d782a080940444f1f0a7d2be81
Hannah Morrison <139557138+hbmorrison@users.noreply.github.com>
2026-03-11 23:26:33 +0000

    Implement core keymap
    commit 8292908fa37c40a8be7f3c04edbb16d8ccebf9f8
    Author: Hannah Morrison <139557138+hbmorrison@users.noreply.github.com>
    Date:   Wed Mar 11 23:23:04 2026 +0000
    
        Use new defines from core_keymap module
    
    commit 5672a36751ff65f9b04dc6d19bee995ef3388946
    Author: Hannah Morrison <139557138+hbmorrison@users.noreply.github.com>
    Date:   Wed Mar 11 23:22:45 2026 +0000
    
        New commits
    
    commit 2dc4bf80bb7f84fa1b2195a3a56e80408e4cb0f6
    Author: Hannah Morrison <139557138+hbmorrison@users.noreply.github.com>
    Date:   Wed Mar 11 09:40:58 2026 +0000
    
        Simplify fills
    
    commit 53bea0c8d32643a3b72c08e7e77c5dd32dbeaa33
    Author: Hannah Morrison <139557138+hbmorrison@users.noreply.github.com>
    Date:   Wed Mar 11 08:58:25 2026 +0000
    
        Use correct layout
    
    commit 2b4f61a6447623427c73920df9261af031561862
    Author: Hannah Morrison <139557138+hbmorrison@users.noreply.github.com>
    Date:   Wed Mar 11 00:27:30 2026 +0000
    
        New commits
    
    commit 057946461da6bc2a69d79441904f66537a221b7a
    Author: Hannah Morrison <139557138+hbmorrison@users.noreply.github.com>
    Date:   Wed Mar 11 00:23:33 2026 +0000
    
        Converted keymaps to use core_keymap module
    
    commit 92e6a8ebc18b57430938c683c9b63eb4f5689e62
    Author: Hannah Morrison <139557138+hbmorrison@users.noreply.github.com>
    Date:   Tue Mar 10 21:53:25 2026 +0000
    
        Fix conflicts

--- keyboards/zilpzalp/keymaps/hbm/keymap.c
+++ keyboards/zilpzalp/keymaps/hbm/keymap.c
@@ -2,170 +2,94 @@
 
 // Module headers.
 
-#include "conditional_layers.h"
+#include "core_keymap.h"
 #include "custom_keys.h"
-
-// Define layers.
-
-enum {
-  LAYER_BS,
-  LAYER_BS_LX,
-  LAYER_BS_RX,
-  LAYER_SM_L,
-  LAYER_SM_LX,
-  LAYER_SM_R,
-  LAYER_SM_RX,
-  LAYER_NM_L,
-  LAYER_NM_LX,
-  LAYER_NV_R,
-  LAYER_NV_RX,
-  LAYER_FN_L,
-  LAYER_FN_LX,
-  LAYER_CT_R,
-  LAYER_MD_L,
-  LAYER_MD_R
-};
-
-// Layer keycodes for primary layers.
-
-#define LT_RS LT(LAYER_SM_R,  KC_R)
-#define LT_RN LT(LAYER_NV_R,  KC_S)
-#define LT_RX LT(LAYER_BS_RX, KC_T)
-#define LT_LX LT(LAYER_BS_LX, KC_N)
-#define LT_LN LT(LAYER_NM_L,  KC_E)
-#define LT_LS LT(LAYER_SM_L,  KC_I)
-#define LT_LM LT(LAYER_MD_L, KC_SPC)
-#define LT_RM LT(LAYER_MD_R, KC_ENT)
-
-// Layer masks for conditional layers.
-
-#define MASK_NM_LX (LAYER_BIT(LAYER_NM_L) | LAYER_BIT(LAYER_BS_LX))
-#define MASK_NV_RX (LAYER_BIT(LAYER_NV_R) | LAYER_BIT(LAYER_BS_RX))
-#define MASK_SM_LX (LAYER_BIT(LAYER_SM_L) | LAYER_BIT(LAYER_BS_LX))
-#define MASK_SM_RX (LAYER_BIT(LAYER_SM_R) | LAYER_BIT(LAYER_BS_RX))
-#define MASK_FN_L  (LAYER_BIT(LAYER_SM_L) | LAYER_BIT(LAYER_NM_L))
-#define MASK_FN_LX (LAYER_BIT(LAYER_SM_L) | LAYER_BIT(LAYER_NM_L) | LAYER_BIT(LAYER_BS_LX))
-#define MASK_CT_R  (LAYER_BIT(LAYER_SM_R) | LAYER_BIT(LAYER_NV_R))
-
-// Add conditional layers.
-
-void add_conditional_layers() {
-
-  // Add the extended number layer, extended nav layer and both extended symbol
-  // layers as conditional layers.
-
-  set_conditional_layer_mask(LAYER_NM_LX, MASK_NM_LX);
-  set_conditional_layer_mask(LAYER_NV_RX, MASK_NV_RX);
-  set_conditional_layer_mask(LAYER_SM_LX, MASK_SM_LX);
-  set_conditional_layer_mask(LAYER_SM_RX, MASK_SM_RX);
-
-  // Add the function layer and its extended layer as conditional layers.
-
-  set_conditional_layer_mask(LAYER_FN_L,  MASK_FN_L);
-  set_conditional_layer_mask(LAYER_FN_LX, MASK_FN_LX);
-
-  // Add the controls layer as a conditional layer.
-
-  set_conditional_layer_mask(LAYER_CT_R,  MASK_CT_R);
-};
 
 // Keymap.
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [LAYER_BS] = LAYOUT(
-        KC_W,  KC_F,  KC_P,   KC_B,  KC_J,  KC_L,  KC_U,    KC_BSPC,
-  KC_A, LT_RS, LT_RN, LT_RX,  KC_G,  KC_M,  LT_LX, LT_LN,   LT_LS,   KC_O,
-        KC_X,  KC_C,  KC_D,                 KC_H,  KC_COMM, KC_DOT,
-                      KC_NO,  LT_LM, LT_RM, KC_NO
+  [LAYER_BASE] = LAYOUT_zilpzalp(
+    /**/           CORE_BASE_TL,  CORE_BASE_IT, CORE_BASE_TR,
+    CORE_BASE_OML, CORE_BASE_ML,  CORE_BASE_IM, CORE_BASE_MR, CORE_BASE_OMR,
+    /**/           CORE_BASE_BL,  /**/          CORE_BASE_BR,
+    /**/           CORE_BASE_THL, CORE_BASE_TH, CORE_BASE_THR
   ),
-  [LAYER_BS_LX] = LAYOUT(
-         KC_Q, KC_NO,  KC_B,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-  KC_NO, KC_A, KC_TAB, KC_G,    KC_NO,   KC_NO,   KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
-         KC_Z, A_ST,   KC_V,                      KC_NO,   KC_NO,   KC_NO,
-                       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+  [LAYER_LEXT] = LAYOUT_zilpzalp(
+    /**/           CORE_EXT_TL,  CORE_FILL_IT, CORE_FILL_TR,
+    CORE_FILL_OML, CORE_EXT_ML,  CORE_FILL_IM, CORE_FILL_MR, CORE_FILL_OMR,
+    /**/           CORE_EXT_BL,  /**/          CORE_FILL_BR,
+    /**/           CORE_EXT_THL, CORE_LEXT_TH, CORE_FILL_THR
   ),
-  [LAYER_BS_RX] = LAYOUT(
-           KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_J,   KC_NO,  KC_Y,
-  KC_NO,   KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,   KC_NO,   KC_M,   KC_ESC, KC_O,    KC_NO,
-           KC_NO,   KC_NO,   KC_NO,                     KC_K,   CK_VC,  KC_SLSH,
-                             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+  [LAYER_REXT] = LAYOUT_zilpzalp(
+    /**/           CORE_FILL_TL,  CORE_FILL_IT, CORE_EXT_TR,
+    CORE_FILL_OML, CORE_FILL_ML,  CORE_FILL_IM, CORE_EXT_MR,  CORE_FILL_OMR,
+    /**/           CORE_FILL_BL,  /**/          CORE_EXT_BR,
+    /**/           CORE_FILL_THL, CORE_REXT_TH, CORE_EXT_THR
   ),
-  [LAYER_SM_L] = LAYOUT(
-         KC_EXLM, CK_AT,   KC_DLR,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-  KC_NO, CK_TILD, KC_LPRN, KC_LCBR, KC_NO,   KC_NO,   KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
-         CK_GRV,  KC_LPRN, KC_RCBR,                   KC_NO,   KC_NO,   KC_NO,
-                           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+  [LAYER_LSYM] = LAYOUT_zilpzalp(
+    /**/          CORE_SYM_TL,   CORE_LSYM_IT, CORE_FILL_TR,
+    CORE_SYM_OML, CORE_SYM_ML,   CORE_LSYM_IM, CORE_FILL_MR, CORE_FILL_OMR,
+    /**/          CORE_SYM_BL,   /**/          CORE_FILL_BR,
+    /**/          CORE_FILL_THL, CORE_FILL_TH, CORE_FILL_THR
   ),
-  [LAYER_SM_LX] = LAYOUT(
-         KC_GRV, KC_HASH, KC_PERC, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-  KC_NO, KC_NO,  KC_NO,   KC_LBRC, KC_NO,   KC_NO,   KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
-         KC_NO,  KC_NO,   KC_RBRC,                   KC_NO,   KC_NO,   KC_NO,
-                          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+  [LAYER_LSYM_EXT] = LAYOUT_zilpzalp(
+    /**/           CORE_SYM_EXT_TL, CORE_FILL_IT, CORE_FILL_TR,
+    CORE_FILL_OML, CORE_SYM_EXT_ML, CORE_FILL_IM, CORE_FILL_MR, CORE_FILL_OMR,
+    /**/           CORE_SYM_EXT_BL, /**/          CORE_FILL_BR,
+    /**/           CORE_FILL_THL,   CORE_FILL_TH, CORE_FILL_THR
   ),
-  [LAYER_SM_R] = LAYOUT(
-         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_AMPR, KC_ASTR, KC_DEL,
-  KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,   KC_NO,   KC_COLN, CK_DQUO, CK_PIPE, KC_NO,
-         KC_NO,   KC_NO,   KC_NO,                     KC_SCLN, KC_QUOT, CK_BSLS,
-                           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+  [LAYER_RSYM] = LAYOUT_zilpzalp(
+    /**/           CORE_FILL_TL,  CORE_RSYM_IT, CORE_SYM_TR,
+    CORE_FILL_OML, CORE_FILL_ML,  CORE_RSYM_IM, CORE_SYM_MR,  CORE_SYM_OMR,
+    /**/           CORE_FILL_BL,  /**/          CORE_SYM_BR,
+    /**/           CORE_FILL_THL, CORE_FILL_TH, CORE_FILL_THR
   ),
-  [LAYER_SM_RX] = LAYOUT(
-         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_CIRC, KC_UNDS, KC_PLUS,
-  KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_MINS, KC_EQL,  KC_NO,
-         KC_NO,   KC_NO,   KC_NO,                     KC_NO,   KC_NO,   KC_NO,
-                           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+  [LAYER_RSYM_EXT] = LAYOUT_zilpzalp(
+    /**/           CORE_FILL_TL,  CORE_FILL_IT, CORE_SYM_EXT_TR,
+    CORE_FILL_OML, CORE_FILL_ML,  CORE_FILL_IM, CORE_SYM_EXT_MR, CORE_FILL_OMR,
+    /**/           CORE_FILL_BL,  /**/          CORE_SYM_EXT_BR,
+    /**/           CORE_FILL_THL, CORE_FILL_TH, CORE_FILL_THR
   ),
-  [LAYER_NM_L] = LAYOUT(
-         KC_1, KC_2, KC_3,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-  KC_NO, KC_4, KC_5, KC_6,    KC_NO,   KC_NO,   KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
-         KC_7, KC_8, KC_9,                      KC_NO,   KC_NO,   KC_NO,
-                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+  [LAYER_NUM] = LAYOUT_zilpzalp(
+    /**/          CORE_NUM_TL,   CORE_NUM_IT,  CORE_FILL_TR,
+    CORE_NUM_OML, CORE_NUM_ML,   CORE_NUM_IM,  CORE_FILL_MR, CORE_FILL_OMR,
+    /**/          CORE_NUM_BL,   /**/          CORE_FILL_BR,
+    /**/          CORE_FILL_THL, CORE_FILL_TH, CORE_FILL_THR
   ),
-  [LAYER_NM_LX] = LAYOUT(
-         KC_NO,   KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-  KC_NO, KC_SLSH, KC_DOT, KC_0,    KC_NO,   KC_NO,   KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
-         KC_NO,   KC_NO,  KC_NO,                     KC_NO,   KC_NO,   KC_NO,
-                          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+  [LAYER_NUM_EXT] = LAYOUT_zilpzalp(
+    /**/           CORE_NUM_EXT_TL, CORE_FILL_IT, CORE_FILL_TR,
+    CORE_FILL_OML, CORE_NUM_EXT_ML, CORE_FILL_IM, CORE_FILL_MR, CORE_FILL_OMR,
+    /**/           CORE_NUM_EXT_BL, /**/          CORE_FILL_BR,
+    /**/           CORE_FILL_THL,   CORE_FILL_TH, CORE_FILL_THR
   ),
-  [LAYER_NV_R] = LAYOUT(
-         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   W_PT,    KC_UP,   W_NT,
-  KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,   KC_NO,   KC_LEFT, KC_DOWN, KC_RIGHT, KC_NO,
-         KC_NO,   KC_NO,   KC_NO,                     AT_PWIN, CK_SS,   AT_NWIN,
-                           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+  [LAYER_NAV] = LAYOUT_zilpzalp(
+    /**/           CORE_FILL_TL,  CORE_NAV_IT, CORE_NAV_TR,
+    CORE_FILL_OML, CORE_FILL_ML,  CORE_NAV_IM, CORE_NAV_MR,  CORE_NAV_OMR,
+    /**/           CORE_FILL_BL,  /**/         CORE_NAV_BR,
+    /**/           CORE_FILL_THL, CORE_FILL_TH, CORE_FILL_THR
   ),
-  [LAYER_NV_RX] = LAYOUT(
-         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   W_PD,    KC_PGUP, W_ND,
-  KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,   KC_NO,   KC_HOME, KC_PGDN, KC_END, KC_NO,
-         KC_NO,   KC_NO,   KC_NO,                     KC_NO,   KC_NO,   KC_NO,
-                           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+  [LAYER_NAV_EXT] = LAYOUT_zilpzalp(
+    /**/           CORE_FILL_TL,  CORE_FILL_IT, CORE_NAV_EXT_TR,
+    CORE_FILL_OML, CORE_FILL_ML,  CORE_FILL_IM, CORE_NAV_EXT_MR, CORE_FILL_OMR,
+    /**/           CORE_FILL_BL,  /**/          CORE_NAV_EXT_BR,
+    /**/           CORE_FILL_THL, CORE_FILL_TH, CORE_FILL_THR
   ),
-  [LAYER_FN_L] = LAYOUT(
-         KC_F1, KC_F2, KC_F3,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,
-  KC_NO, KC_F4, KC_F5, KC_F6,   KC_NO,   KC_NO,   KC_TRNS,  KC_TRNS, KC_TRNS, KC_NO,
-         KC_F7, KC_F8, KC_F9,                     KC_NO,    KC_NO,   KC_NO,
-                       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+  [LAYER_FUNC] = LAYOUT_zilpzalp(
+    /**/           CORE_FUNC_TL,  CORE_FUNC_IT, CORE_FILL_TR,
+    CORE_FUNC_OML, CORE_FUNC_ML,  CORE_FUNC_IM, CORE_FILL_MR, CORE_FILL_OMR,
+    /**/           CORE_FUNC_BL,  /**/          CORE_FILL_BR,
+    /**/           CORE_FILL_THL, CORE_FILL_TH, CORE_FILL_THR
   ),
-  [LAYER_FN_LX] = LAYOUT(
-         KC_NO,  KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-  KC_NO, KC_F10, KC_F11, KC_F12,  KC_NO,   KC_NO,   KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
-         KC_NO,  KC_NO,  KC_NO,                     KC_NO,   KC_NO,   KC_NO,
-                         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+  [LAYER_FUNC_EXT] = LAYOUT_zilpzalp(
+    /**/           CORE_FUNC_EXT_TL, CORE_FILL_IT, CORE_FILL_TR,
+    CORE_FILL_OML, CORE_FUNC_EXT_ML, CORE_FILL_IM, CORE_FILL_MR, CORE_FILL_OMR,
+    /**/           CORE_FUNC_EXT_BL, /**/          CORE_FILL_BR,
+    /**/           CORE_FILL_THL,    CORE_FILL_TH, CORE_FILL_THR
   ),
-  [LAYER_CT_R] = LAYOUT(
-         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_MUTE, KC_MPLY, KC_PSCR,
-  KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,   KC_NO,   KC_VOLU, KC_MNXT, KC_BRIU, KC_NO,
-         KC_NO,   KC_NO,   KC_NO,                     KC_VOLD, KC_MPRV, KC_BRID,
-                           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
-  ),
-  [LAYER_MD_L] = LAYOUT(
-         KC_NO,   DM_RGUI, KC_NO,   KC_NO,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_NO, DM_RALT, DM_RCTL, DM_RSFT, KC_NO,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-         KC_NO,   KC_NO,   KC_NO,                     KC_TRNS, KC_TRNS, KC_TRNS,
-                           KC_TRNS, KC_TRNS, CW_TOGG, KC_TRNS
-  ),
-  [LAYER_MD_R] = LAYOUT(
-           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,   KC_NO,   DM_LGUI, KC_NO,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,   DM_LSFT, DM_LCTL, DM_LALT, KC_NO,
-           A_CX,    A_CC,    A_CV,                      KC_NO,   KC_NO,   KC_NO,
-                             KC_TRNS, CW_TOGG, KC_TRNS, KC_TRNS
+  [LAYER_CTLS] = LAYOUT_zilpzalp(
+    /**/           CORE_FILL_TL,  CORE_CTLS_IT, CORE_CTLS_TR,
+    CORE_FILL_OML, CORE_FILL_ML,  CORE_CTLS_IM, CORE_CTLS_MR, CORE_CTLS_OMR,
+    /**/           CORE_FILL_BL,  /**/          CORE_CTLS_BR,
+    /**/           CORE_FILL_THL, CORE_FILL_TH, CORE_FILL_THR
   )
 };
